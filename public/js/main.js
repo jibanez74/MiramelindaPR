b1a51664927e6ef51a28728b1202f92ba8351644
Jose Ibanez <jose.ibanez@blindpenguincoder.com>
2024-02-25 21:08:49 -0400

    now running an express server using bun

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // Add smooth scrolling
+  $("#main-nav a").on("click", function (e) {
+    // Check for a hash value
+    if (this.hash !== "") {
+      // Prevent default behavior
+      e.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // add scroll spy
+      $("body").scrollspy({ target: "#main-nav" });
+
+      // Animate smooth scroll
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        900,
+        function () {
+          // Add hash to URL after scroll
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
